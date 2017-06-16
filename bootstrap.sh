ced7cab6ef2e3ebcaec77e2c9387ea42962ff32b
sykwer <sykwer@gmail.com>
2017-06-16 18:29:30 +0900

    Add clone or update repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,11 +28,45 @@ print_header() {
  } | sed -e 's/8/🔥/g'
 }
 
+clone_or_update_repo() {
+  local git_dir="$DOTFILES_PATH/.git"
+
+  if [ -d "$git_dir" ]; then
+    echo 'Updating repo...'
+
+    if [ "$(git --git-dir="$git_dir" symbolic-ref --short HEAD 2> /dev/null)" != "master" ]; then
+      echo 'Skip repo update. You are working on a non-master branch.'
+      return
+    fi
+
+    if ! git --git-dir="$git_dir" diff --no-ext-diff --quiet --exit-code 2> /dev/null 2>&1; then
+      echo 'Skip repo update. There are unstaged changes.'
+      return
+    fi
+
+    if ! git --git-dir="$git_dir" diff-index --cached --quiet HEAD -- >/dev/null 2>&1; then
+      echo 'Skip repo update. There are uncommited changes.'
+      return
+    fi
+
+    git --git-dir="$git_dir" pull origin master
+    git --git-dir="$git_dir" submodule sync
+    echo 'done'
+  elif ! [ -d "$DOTFILES_PATH" ]; then
+    echo 'Cloning repo...'
+    git clone --recursive https://github.com/sykwer/dotfiles.git --branch $DOTFILES_BRANCH $DOTFILES_PATH
+    echo 'done'
+  else
+    echo 'Stop cloning repo. You are supposed to use ~/dotfiles directory for manage dotfiles. Please make directory ~/dotfiles.'
+    exit 1
+  fi
+}
 
 #=== EntryPoint
 #====================================================================================================
 main() {
   print_header
+  clone_or_update_repo
 }
 
 main
